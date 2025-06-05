06f1ebdd9fc445bb68d1ccc12c8b41e8f4e88503
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-06-05 19:53:31 +0300

    use @Autowired for dependency injection

--- src/main/java/com/selah/store/OrderService.java
+++ src/main/java/com/selah/store/OrderService.java
@@ -1,12 +1,17 @@
 package com.selah.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-    private final PaymentService paymentService;
-    OrderService(PaymentService paymentService){
+    private  PaymentService paymentService;
+    public OrderService(PaymentService paymentService, int x){
+
+    }
+    @Autowired
+    public OrderService(PaymentService paymentService){
     this.paymentService=paymentService;
     }
     public void placeOrder() {
