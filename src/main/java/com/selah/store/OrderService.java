716d6a1119af9523a081611d37b406931faace9d
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-06-05 19:45:21 +0300

    using @Service annotations to define services

--- src/main/java/com/selah/store/OrderService.java
+++ src/main/java/com/selah/store/OrderService.java
@@ -1,5 +1,9 @@
 package com.selah.store;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private final PaymentService paymentService;
     OrderService(PaymentService paymentService){
